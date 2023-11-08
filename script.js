3a2a23696393277444ed75942c7f200a30336469
Eduardo Centenaro <centenaro57@gmail.com>
2023-11-08 14:10:56 -0300

    Change execution time and remove inserted numbers

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let numerosInseridos = [];
 let numerosOrdenados = [];
 
 function ordenarNumerosInsertion() {
+  const startTime = performance.now();
+
   const input = document.getElementById('numberInput');
   const numerosSpan = document.getElementById('numerosSpan');
   const ordenadosSpan = document.getElementById('ordenadosSpan');
@@ -9,10 +10,14 @@ function ordenarNumerosInsertion() {
   const inputValues = input.value.trim().split(' ');
 
   numerosInseridos = inputValues.map(value => parseFloat(value)).filter(value => !isNaN(value));
-  numerosSpan.textContent = numerosInseridos.join(', ');
 
   numerosOrdenados = insertionSort([...numerosInseridos]);
   ordenadosSpan.textContent = numerosOrdenados.join(', ');
+
+  const endTime = performance.now();
+  const executionTime = endTime - startTime;
+
+  console.log(`Executado em ${executionTime} milissegundos.`);
 }
 
 function insertionSort(arr) {
@@ -26,23 +31,4 @@ function insertionSort(arr) {
     arr[j + 1] = current;
   }
   return arr;
-}
-
-function medirTempo() {
-  // Início da medição
-  const inicio = performance.now();
-
-  // Seu código aqui (simulação de um código que leva algum tempo para executar)
-  for (let i = 0; i < 1000000; i++) {
-    Math.sqrt(i);
-  }
-
-  // Fim da medição
-  const fim = performance.now();
-
-  const tempoExecucao = fim - inicio;
-
-  // Exibindo o tempo de execução na tela
-  const resultadoElement = document.getElementById('resultado');
-  resultadoElement.textContent = `Tempo de execução: ${tempoExecucao.toFixed(3)} ms`;
 }
